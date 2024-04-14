8b6db653c2906e54294fea0aef56a5e023226e56
SANTIAGO0120 <u20201187651@usco.edu.co>
2024-04-13 21:47:36 -0500

    Actualizado

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,26 +8,24 @@
 <body>
     
 <x-guest-layout>
-<form method="POST" action="{{ route('register') }}" class="formulario__register">
+<div class="contenedor__todo">
+   <form method="POST" action="{{ route('register') }}" class="formulario__register">
         @csrf
 
         <h2>Registrarse</h2>
-
-        <!-- Nombre de usuario -->
+        
         <div>
             <x-input-label for="name" :value="__('Nombre de usuario')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
-        <!-- Correo Electrónico -->
         <div class="mt-4">
             <x-input-label for="email" :value="__('Correo Electrónico')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <!-- Contraseña -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Contraseña')" />
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required />
@@ -40,21 +38,16 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <!-- Nombre -->
         <div class="mt-4">
             <x-input-label for="first_name" :value="__('Nombre')" />
             <x-text-input id="first_name" class="block mt-1 w-full" type="text" name="first_name" :value="old('first_name')" />
-            <!-- Se omite manejo de errores para nombre -->
         </div>
 
-        <!-- Apellidos -->
         <div class="mt-4">
             <x-input-label for="last_name" :value="__('Apellidos')" />
             <x-text-input id="last_name" class="block mt-1 w-full" type="text" name="last_name" :value="old('last_name')" />
-            <!-- Se omite manejo de errores para apellidos -->
         </div>
 
-        <!-- Tipo de Identificación -->
         <div class="mt-4">
             <x-input-label for="idtype" :value="__('Tipo de Identificación')" />
             <select class="idtype block mt-1 w-full" id="idtype" name="identification_type">
@@ -63,28 +56,22 @@
                 <option value="ce">Cédula de Extranjería</option>
                 <option value="ti">Tarjeta de Identidad</option>
             </select>
-            <!-- Se omite manejo de errores para tipo de identificación -->
         </div>
 
-        <!-- Número de Identificación -->
         <div class="mt-4">
             <x-input-label for="identification_number" :value="__('N. Identificación')" />
             <x-text-input id="identification_number" class="block mt-1 w-full" type="text" name="identification_number" :value="old('identification_number')" />
-            <!-- Se omite manejo de errores para número de identificación -->
         </div>
 
-        <!-- Número de Celular -->
         <div class="mt-4">
             <x-input-label for="phone_number" :value="__('N. Celular')" />
             <x-text-input id="phone_number" class="block mt-1 w-full" type="number" name="phone_number" :value="old('phone_number')" />
-            <!-- Se omite manejo de errores para número de celular -->
         </div>
 
-        <!-- Dirección -->
         <div class="mt-4">
             <x-input-label for="address" :value="__('Dirección')" />
             <x-text-input id="address" class="block mt-1 w-full" type="text" name="address" :value="old('address')" />
-            <!-- Se omite manejo de errores para dirección -->
+        
         </div>
 
         <div class="flex items-center justify-end mt-4">
@@ -93,6 +80,7 @@
             </x-primary-button>
         </div>
     </form>
+    </div>
 </x-guest-layout>
 
 </body>
