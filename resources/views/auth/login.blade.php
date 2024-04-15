c1e7dea7bf363b2b7baa70e01b1a83dd216f6f04
SANTIAGO0120 <u20201187651@usco.edu.co>
2024-04-15 10:58:01 -0500

    Se implementa el js correctamente

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login y Register - Guadalupana</title>
-    
+
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link href="{{ asset('asset/login.css') }}" rel="stylesheet">
 </head>
@@ -45,6 +45,6 @@
     </div>
 </main>
 
-<script src="{{ asset('js/script.js') }}"></script>
+<script src="{{ asset('asset/js/script.js') }}"></script>
 </body>
 </html>
