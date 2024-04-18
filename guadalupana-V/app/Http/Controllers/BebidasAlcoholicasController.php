45a308a368145006904f4ec8f10d10ba81efa7d6
Deily13 <quinceyton.17@gamil.com>
2024-04-18 18:00:55 -0500

    error de direcionamiento de la vista bebidas alcoholicas

--- guadalupana-V/app/Http/Controllers/BebidasAlcoholicasController.php
+++ guadalupana-V/app/Http/Controllers/BebidasAlcoholicasController.php
@@ -4,8 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class BebidasAlcholicasController extends Controller
+class BebidasAlcoholicasController extends Controller
 {
+    //
     public function index()
     {
         return view('/auth/bebidas-alcoholicas');
