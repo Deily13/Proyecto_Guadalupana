07d8aba978fc530fec9f3780a14cdd296c4eeefb
SANTIAGO0120 <u20201187651@usco.edu.co>
2024-04-23 23:12:35 -0500

    Implementación de responsive para dispositivos moviles

--- public/asset/js/Productos.js
+++ public/asset/js/Productos.js
@@ -2,16 +2,13 @@ function toggleDropdown() {
     var dropdown = document.getElementById("dropdownMenu");
     if (dropdown.style.display === "none") {
         dropdown.style.display = "block";
-        // Agrega un controlador de eventos al documento para escuchar clics fuera del menú
         document.addEventListener('click', closeDropdownOnClickOutside);
     } else {
         dropdown.style.display = "none";
-        // Elimina el controlador de eventos del documento cuando el menú se oculta
         document.removeEventListener('click', closeDropdownOnClickOutside);
     }
 }
 
-// Esta función oculta el menú desplegable si se hace clic fuera de él
 function closeDropdownOnClickOutside(event) {
     var dropdown = document.getElementById("dropdownMenu");
     if (!dropdown.contains(event.target)) {
@@ -24,16 +21,15 @@ document.querySelector('.menu').addEventListener('click', function(event) {
     event.stopPropagation();
 });
 
-
 function calificar(seccion, calificacion) {
     var calificacionDiv = document.getElementById("calificacion".concat(seccion));
     var estrellas = calificacionDiv === null || calificacionDiv === void 0 ? void 0 : calificacionDiv.querySelectorAll('.estrella');
     estrellas === null || estrellas === void 0 ? void 0 : estrellas.forEach(function (estrella, index) {
         if (index < calificacion) {
-            estrella.style.color = 'gold'; 
+            estrella.style.color = 'gold';
         }
         else {
-            estrella.style.color = 'black'; 
+            estrella.style.color = 'black';
         }
     });
 }
@@ -43,7 +39,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     botonesPedir.forEach(function(boton) {
         boton.addEventListener('click', function(event) {
-        
+
             event.preventDefault();
             boton.textContent = 'Seleccionado';
             boton.disabled = true;
@@ -59,36 +55,40 @@ checkboxes.forEach(function(checkbox) {
         var checkedCount = document.querySelectorAll('.SaborCheckbox-button:checked').length;
         if (checkedCount > limite) {
             this.checked = false;
-            alert('Solo puedes seleccionar hasta ' + limite + ' opciones.');
+            new Noty({
+                type: 'error',
+                layout: 'topRight',
+                text: 'Solo puedes seleccionar hasta ' + limite + ' opciones.'
+            }).show();
         }
     });
 });
 
 function makeResponsive() {
     var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    var toolbar = document.querySelector('.toolbar');
+    var botonera = document.querySelector('.botonera');
+    var containerProductos = document.querySelector('.ContainerProductos');
+    var cuadros = document.querySelectorAll('.Cuadro');
+
     if (width <= 768) {
-        // Apply mobile styles
-        document.querySelector('.toolbar').style.width = '100%';
-        document.querySelector('.toolbar').style.margin = '0';
-        document.querySelector('.botonera').style.width = '100%';
-        document.querySelector('.ContainerProductos').style.flexDirection = 'column';
-        document.querySelectorAll('.Cuadro').forEach(function(cuadro) {
+        toolbar.style.width = '100%';
+        toolbar.style.margin = '0';
+        botonera.style.width = '100%';
+        containerProductos.style.flexDirection = 'column';
+        cuadros.forEach(function(cuadro) {
             cuadro.style.width = '100%';
         });
     } else {
-        // Apply desktop styles
-        document.querySelector('.toolbar').style.width = '96%';
-        document.querySelector('.toolbar').style.margin = '2%';
-        document.querySelector('.botonera').style.width = '30%';
-        document.querySelector('.ContainerProductos').style.flexDirection = 'row';
-        document.querySelectorAll('.Cuadro').forEach(function(cuadro) {
+        toolbar.style.width = '96%';
+        toolbar.style.margin = '2%';
+        botonera.style.width = '30%';
+        containerProductos.style.flexDirection = 'row';
+        cuadros.forEach(function(cuadro) {
             cuadro.style.width = 'calc(45% - 2px)';
         });
     }
 }
 
-// Add event listener for window resize
 window.addEventListener('resize', makeResponsive);
-
-// Call the function on initial load
 makeResponsive();
