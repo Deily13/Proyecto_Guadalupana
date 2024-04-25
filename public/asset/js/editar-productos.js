cd48c377c7abc1b7413753e5afbb71aee1184818
Deily13 <quinceyton.17@gamil.com>
2024-04-24 19:45:16 -0500

    mejora de botones

--- public/asset/js/editar-productos.js
+++ public/asset/js/editar-productos.js
@@ -1,32 +1,27 @@
 // Función para abrir el menú lateral
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
-    // Ajusta el margen izquierdo y la anchura del contenido principal para centrarlo
     document.getElementById("main").style.marginLeft = "250px";
     document.getElementById("main").style.width = "calc(100% - 250px)";
     document.getElementById("main").style.transition = "margin-left 0.5s";
-    // Muestra el botón de cierre siempre que el menú esté abierto
-    document.getElementById("closebtn").style.display = "block"; // Modificado
+    document.getElementById("closebtn").style.display = "block"; 
 }
 
 // Función para cerrar el menú lateral
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
-    // Restablece el margen y la anchura del contenido principal
     document.getElementById("main").style.marginLeft = "0";
     document.getElementById("main").style.width = "100%";
     document.getElementById("main").style.transition = "margin-left 0.5s";
-    // Oculta el botón de cierre cuando el menú está cerrado
-    document.getElementById("closebtn").style.display = "none"; // Modificado
+    document.getElementById("closebtn").style.display = "none";
 }
 
 // Añade un detector de eventos de cambio de tamaño de ventana
 window.addEventListener("resize", function (event) {
     if (window.innerWidth > 1080) {
-        // Si la ventana es más ancha que 1080px y el menú está abierto, muestra el botón de cierre
         var sidebarWidth = document.getElementById("mySidebar").style.width;
         if (sidebarWidth === "250px") {
-            document.getElementById("closebtn").style.display = "block"; // Modificado
+            document.getElementById("closebtn").style.display = "block";
         }
     } else {
         closeNav();
@@ -170,8 +165,10 @@ function transformToEditForm(productCard) {
           <label>Características:</label>
           <textarea class="form-control" name="productFeatures">${currentFeatures}</textarea>
         </div>
+        <div class="button-container">
         <button type="button" class="btn btn-success save-btn">Guardar</button>
         <button type="button" class="btn btn-secondary cancel-btn">Cancelar</button>
+        </div>
       `;
 
     productCard.innerHTML = "";
@@ -215,7 +212,7 @@ function saveEditedProduct(productCard, form, currentImageSrc) {
 // Asigna eventos a los botones existentes al cargar la página
 assignButtonEvents();
 
-// ... (el resto de tu código)
+
 
 document.addEventListener("DOMContentLoaded", function () {
     assignButtonEvents();
